853f1ff24c3d32f84d17c7d18f41167cdcd7ab84
John Connor <zkmd2012@inbox.ru>
2021-10-31 18:12:06 +0500

    2 Signup and Login forms with styles created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,38 @@
 import s from './Login.module.css'
+import { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password)
+    }
+
     return (
-        <h1>Login</h1>
+        <form onSubmit={handleSubmit} className={s['login-form']}> 
+            <h2>Login</h2>
+            <label>
+                <span>email:</span>
+                <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+
+            <label>
+                <span>password:</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className="btn">Login</button>
+        </form>
     )
 }
 
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,8 +1,48 @@
+import { useState } from 'react'
 import s from './Signup.module.css'
 
 const Signup = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, email, password)
+    }
+
     return (
-        <h1>Signup</h1>
+        <form onSubmit={handleSubmit} className={s['signup-form']}> 
+            <h2>Signup</h2>
+
+            <label>
+                <span>name:</span>
+                <input
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+            </label>
+
+            <label>
+                <span>email:</span>
+                <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+
+            <label>
+                <span>password:</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className="btn">Signup</button>
+        </form>
     )
 }
 
