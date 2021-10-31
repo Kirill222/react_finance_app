4156c6c65491419e338bab027217cbb21c872b35
John Connor <zkmd2012@inbox.ru>
2021-10-31 20:08:07 +0500

    3 FIREBASE/FIRESTORE SETUP

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCel_YOxnb5WQuH7VVcfKwkJiLSsjQGE7E",
+    authDomain: "react-finance-app-69a4e.firebaseapp.com",
+    projectId: "react-finance-app-69a4e",
+    storageBucket: "react-finance-app-69a4e.appspot.com",
+    messagingSenderId: "660997969910",
+    appId: "1:660997969910:web:444b22295af3e89780d9d7"
+  }
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init services
+  const projectFirestore = firebase.firestore()
+
+  export {projectFirestore}
