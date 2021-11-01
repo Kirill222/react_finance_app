b1245526012797b18686e10304fd108c8da919d6
John Connor <zkmd2012@inbox.ru>
2021-11-01 15:40:35 +0500

    13 Conditionally show user content depending on whether the user is logged in or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import s from './Navbar.module.css'
 import { useLogoutFirebase } from '../hooks/useLogoutFirebase'
-
+import { useAuthContext } from '../hooks/useAuthContext'
+import { AuthContext } from '../context/AuthContext'
 import {Link} from 'react-router-dom'
 
 const Navbar = () => {
@@ -8,18 +9,31 @@ const Navbar = () => {
 
     const { logout } = useLogoutFirebase()
 
+    const {user} = useAuthContext()
 
     return (
         <nav className={s.navbar}>
             <ul>
                 <li className={s.title}>myMoney</li>
 
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
 
-                <li>
-                    <button className="btn" onClick={logout}>Logout</button>
-                </li>
+                {!user && (
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/signup">Signup</Link></li>
+                    </>
+                )}
+                
+
+                {user && (
+                    <>
+                        <li>Hello, {user.displayName}</li>
+                        <li>
+                            <button className="btn" onClick={logout}>Logout</button>
+                        </li>
+                    </>
+                )}
+                
             </ul>
         </nav>
     )
