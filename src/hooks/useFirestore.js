308734d4b4b19dbcfa571ab57695d4e10e3c0013
John Connor <zkmd2012@inbox.ru>
2021-11-01 20:25:56 +0500

    19 useFirestore hook - adding logic to add a document

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,6 +10,12 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
     switch (action.type) {
+        case 'IS_PENDING':
+            return {...state, isPending: true, document: null, success: false, error: null}
+        case 'ADDED_DOCUMENT':
+            return {...state, isPending: false, document: action.payload, success: true, error: null}
+        case 'ERROR':
+            return {...state, isPending: false, document: null, success: false, error: action.payload}
         default:
             return state
     }
@@ -22,12 +28,29 @@ export const useFireStore = (collection) => {
 
     //collection ref
     const ref = projectFirestore.collection(collection)
+
+    //only dispatch if not cancelled
+    const dispatchIfNotCancelled = (action) => {
+        if (!isCancelled) {
+            dispatch(action)
+        }
+    }
+
     //add a document
-    const addDocument = (doc) => {
+    const addDocument = async (doc) => {
+        dispatch({type: 'IS_PENDING'})
+
+        try {
+            const addedDocument = await ref.add(doc) 
+            dispatchIfNotCancelled({type: 'ADDED_DOCUMENT', payload: addedDocument})           
+        } 
+        catch (err) {
+            dispatchIfNotCancelled({type: 'ERROR', payload: err.message})
+        }
 
     }
     //delete a document
-    const deleteDocument = (id) => {
+    const deleteDocument = async (id) => {
 
     }
 
@@ -35,5 +58,7 @@ export const useFireStore = (collection) => {
     useEffect(() => {
         return () => setIsCancelled(true)
     }, [])
+
+    return {addDocument, deleteDocument, response}
 
 }
