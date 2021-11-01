a634645c28d74332bcb0708adffb975bfd0e3d7f
John Connor <zkmd2012@inbox.ru>
2021-11-01 12:46:10 +0500

    12 Using Login Hook

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,17 @@
 import s from './Login.module.css'
 import { useState } from 'react'
+import { useLoginFirebase } from '../../hooks/useLoginFirebase'
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const {login, error, isPending} = useLoginFirebase()
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email, password)
+        login(email, password)
     }
 
     return (
@@ -31,7 +34,9 @@ const Login = () => {
                     value={password}
                 />
             </label>
-            <button className="btn">Login</button>
+            {!isPending && <button className="btn">Login</button>}
+            {isPending && <button disabled className="btn">loading</button>}
+            {error && <p>{error}</p>}
         </form>
     )
 }
